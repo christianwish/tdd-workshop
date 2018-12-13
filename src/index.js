c0312a914e036f52d620815a990a93ed9226f21b
Christian <chris.heyn.dd@gmail.com>
2018-12-13 08:13:45 +0100

    prepare

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import './index.css';
 import $ from 'jquery';
 
 const $form = $('form');
+const $inputs = $form.find('input');
 
 const setForm = (status) => {
-    $form.removeClass(status);
+    $form.attr('class', '');
     setTimeout(() => $form.addClass(status), 0);
 };
 
+$inputs.on('keydown', () => setForm(''));
+
 $form.on('submit', (event) => {
     event.preventDefault();
+    const isAnagram = false;
+
+    if (isAnagram) {
+        setForm('valid');
+        return undefined;
+    }
+
     setForm('invalid');
+    return undefined;
 });
